d6dea5d6f27b5986287db7be87749117a2a4c1a6
Esmael <esmael6360@gmail.com>
2026-06-02 13:02:29 +0300

    comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+// This is the entry point of the React application. It imports necessary modules and styles, and renders the main `App` component inside a `StrictMode` wrapper for highlighting potential issues in the application. The `createRoot` function from React DOM is used to create a root for rendering the app, targeting the HTML element with the ID of 'root'.
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
