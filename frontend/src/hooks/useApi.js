0c9ea600ae82987a407ce5dad44a2698bbac948a
Esmael <esmael6360@gmail.com>
2026-06-02 12:55:26 +0300

    removed

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-// This custom hook provides a standardized way to handle API calls in the application. It manages loading and error states, allowing components to easily call APIs and handle responses without having to repeat the same logic for each API call. The `callApi` function takes an API call as an argument, executes it, and updates the loading and error states accordingly. It also provides a `clearError` function to reset the error state when needed.
 export const useApi = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
